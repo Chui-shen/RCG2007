269e7995ca3ca4fcdc6afedde6385c42c64111a8
ChuishenX <chuishenx@oblivionocean.top>
2024-03-24 14:26:51 +0800

    Translation to English and Trebuchet MS

--- ui.py
+++ ui.py
@@ -3,15 +3,21 @@
 ################################################################################
 ## Form generated from reading UI file 'untitled.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+## Created by: Qt User Interface Compiler version 6.5.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QPlainTextEdit,
+    QPushButton, QSizePolicy, QStatusBar, QTextBrowser,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -27,21 +33,21 @@ class Ui_MainWindow(object):
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(9, 9, 192, 72))
         font = QFont()
-        font.setFamily(u"\u54e5\u7279\u5f0f\u5b57\u4f53")
+        font.setFamilies([u"Trebuchet MS"])
         font.setPointSize(48)
         self.label.setFont(font)
         self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
         self.pushButton.setGeometry(QRect(11, 308, 481, 34))
         font1 = QFont()
-        font1.setFamily(u"\u65b9\u6b63\u7c97\u9ed1\u5b8b\u7b80\u4f53")
+        font1.setFamilies([u"Trebuchet MS"])
         font1.setPointSize(16)
         self.pushButton.setFont(font1)
         self.outputer = QTextBrowser(self.centralwidget)
         self.outputer.setObjectName(u"outputer")
         self.outputer.setGeometry(QRect(10, 201, 482, 101))
         font2 = QFont()
-        font2.setFamily(u"\u65b9\u6b63\u7c97\u9ed1\u5b8b\u7b80\u4f53")
+        font2.setFamilies([u"Trebuchet MS"])
         font2.setPointSize(28)
         self.outputer.setFont(font2)
         self.outputer.setCursorWidth(0)
@@ -49,14 +55,14 @@ class Ui_MainWindow(object):
         self.label_2.setObjectName(u"label_2")
         self.label_2.setGeometry(QRect(9, 87, 120, 25))
         font3 = QFont()
-        font3.setFamily(u"\u65b9\u6b63\u7c97\u9ed1\u5b8b\u7b80\u4f53")
+        font3.setFamilies([u"Trebuchet MS"])
         font3.setPointSize(15)
         self.label_2.setFont(font3)
         self.inputer = QPlainTextEdit(self.centralwidget)
         self.inputer.setObjectName(u"inputer")
         self.inputer.setGeometry(QRect(9, 118, 482, 41))
         font4 = QFont()
-        font4.setFamily(u"\u65b9\u6b63\u7c97\u9ed1\u5b8b\u7b80\u4f53")
+        font4.setFamilies([u"Trebuchet MS"])
         font4.setPointSize(18)
         self.inputer.setFont(font4)
         self.label_3 = QLabel(self.centralwidget)
@@ -83,8 +89,8 @@ class Ui_MainWindow(object):
         self.label_4.setObjectName(u"label_4")
         self.label_4.setGeometry(QRect(330, 60, 41, 41))
         font5 = QFont()
-        font5.setFamily(u"\u65b9\u6b63\u7c97\u9ed1\u5b8b\u7b80\u4f53")
-        font5.setPointSize(48)
+        font5.setFamilies([u"Trebuchet MS"])
+        font5.setPointSize(26)
         self.label_4.setFont(font5)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
@@ -97,19 +103,19 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"\u6447\u4eba\u673a", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"\u6447\u4eba\u673a", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u6447\u4eba\u55bd", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"RCG", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"RCG", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Start", None))
         self.outputer.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'\u65b9\u6b63\u7c97\u9ed1\u5b8b\u7b80\u4f53'; font-size:28pt; font-weight:400; font-style:normal;\">\n"
+"</style></head><body style=\" font-family:'Trebuchet MS'; font-size:28pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u8981\u6447\u51e0\u4e2a\u4eba\uff1a", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Number: ", None))
         self.inputer.setPlainText(QCoreApplication.translate("MainWindow", u"1", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u6447\u51fa\u6765\u7684\u4eba\uff1a", None))
-        self.first.setText(QCoreApplication.translate("MainWindow", u"\u521d\u6b21\u542f\u52a8\u8bf7\u70b9\u51fb", None))
-        self.generator.setText(QCoreApplication.translate("MainWindow", u"\u751f\u6210\u6570\u5217", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Chosen Ones: ", None))
+        self.first.setText(QCoreApplication.translate("MainWindow", u"Initiation", None))
+        self.generator.setText(QCoreApplication.translate("MainWindow", u"Generate", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"~", None))
     # retranslateUi
 
